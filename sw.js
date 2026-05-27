cf01496732f433c36cc89ca71c9c5bce79e99dcc
SatiiixX <dgi.zgi24@gmail.com>
2026-05-28 00:02:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache-Version hochzählen erzwingt komplette Neu-Installation
-const C = 'ag-v5-0527214235';
+const C = 'ag-v9-0527220151';
 const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
